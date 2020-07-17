acb2195248f29a87733277734555a20048f1af4c
Troels Laegsgaard <troels.laegsgaard@maersk.com>
2020-07-17 17:28:34 +0200

    Changed package version.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='powertools',
-    version='0.1',
-    description='Python data transformation project',
+    version='0.0.1',
+    description='PySpark data transformation project.',
     packages=['powertools'],
 )
