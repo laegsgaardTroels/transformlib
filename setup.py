06d9ed515bf892cde8dea3650a2ec9fb53580de6
Troels Laegsgaard <troels.laegsgaard@gmail.com>
2020-09-10 17:57:13 +0200

    Added minimum python version.

--- setup.py
+++ setup.py
@@ -5,10 +5,11 @@ setup(
     name='powertools',
     version='0.0.1',
     description=(
-        "Enables the user to organize transformations of data with PySpark as a regular Python"
-        "package."
+        "Enables the user to organize transformations of data with "
+        "PySpark as a regular Python package."
     ),
     packages=['powertools'],
+    python_requires='>=3.7',
     install_requires=[
         'pandas==1.0.4',
         'pyarrow==0.15.1',
