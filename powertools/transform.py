4339a57ce349200988bd61450a4c48b302a64d15
Troels Laegsgaard <troels.laegsgaard@gmail.com>
2021-01-09 19:22:14 +0100

    noqa

--- powertools/transform.py
+++ powertools/transform.py
@@ -63,7 +63,7 @@ class Transform:
         >>> from pyspark.sql import functions as F
         >>> func = lambda a, b: a.write(b.read().agg(F.count('*')))
         >>> transform(a=Output('foo'), b=Input('bar'))(func)
-        <class 'powertools.transform.Transform'>(func=<function <lambda> at 0x7f79251df4c0>, a=<class 'powertools.node.Output'>(path=/tmp/foo), b=<class 'powertools.node.Input'>(path=/tmp/bar))
+        <class 'powertools.transform.Transform'>(func=<function <lambda> at 0x7f79251df4c0>, a=<class 'powertools.node.Output'>(path=/tmp/foo), b=<class 'powertools.node.Input'>(path=/tmp/bar))  # noqa
 
         Returns:
             Dict[Node]: The key value arguments to the function.
