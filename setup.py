101a88b4f88e8962a94ea16ec47635b5d1e07654
Troels Laegsgaard <troels.laegsgaard@gmail.com>
2021-01-10 11:12:34 +0100

    Added description to setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,11 @@
+import pathlib
 from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
 
 
 setup(
@@ -8,6 +15,9 @@ setup(
         "Enables the user to organize transformations of data with "
         "PySpark as a regular Python package."
     ),
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/laegsgaardTroels/powertools",
     packages=['powertools'],
     python_requires='>=3.7',
     install_requires=[
