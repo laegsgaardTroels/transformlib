28a5183790919019a9cba03b4a430e1652404731
Troels Laegsgaard <laegsgaardtroels@gmail.com>
2024-03-04 20:56:32 +0100

    Updates workflows

--- setup.py
+++ setup.py
@@ -35,18 +35,16 @@ setup(
         where='src',
         include=['transformlib*'],
     ),
-    python_requires='>=3.9',
+    python_requires='>=3.10',
     install_requires=[],
     extras_require={
         'dev': [
             'flake8==3.8.3',
             'jinja2==3.0.0',
-            'pydata-sphinx-theme',
-            'pyspark==3.0.0',
             'pytest-cov',
             'pytest-pep8',
-            'pytest==5.3.5',
-            'sphinx==3.2.1',
+            'pytest==8.0.2',
+            'sphinx==7.2.6',
         ]
     },
     entry_points={
