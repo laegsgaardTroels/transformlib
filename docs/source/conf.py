78f73d70f7ceaec37ebe1f4942b43554ee4807ee
Troels Laegsgaard <laegsgaardtroels@gmail.com>
2024-03-04 21:02:09 +0100

    Fixing workflows

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,7 @@
 #
 import os
 import sys
-from transformlib import __version__
+from setup import __version__
 sys.path.insert(0, os.path.abspath('.'))
 
 # -- Project information -----------------------------------------------------
