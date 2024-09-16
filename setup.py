2abf16e2c4b136211448d555966492b480bdfcc1
Troels Laegsgaard <laegsgaardtroels@gmail.com>
2024-09-16 10:38:52 +0200

    Bumps minor and adds optional feature installs [all] and [pandas]

--- setup.py
+++ setup.py
@@ -12,17 +12,18 @@ README = (HERE / "README.md").read_text()
 VERSIONFILE = "src/transformlib/__init__.py"
 
 try:
-    __version__ = (
-        re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", open(VERSIONFILE, "rt").read(), re.M)
-        .group(1)
-    )
+    __version__ = re.search(
+        r"^__version__ = ['\"]([^'\"]*)['\"]", open(VERSIONFILE,
+                                                    "rt").read(), re.M
+    ).group(1)
 except Exception as exception:
-    raise RuntimeError(f"Unable to find version string in {VERSIONFILE}") from exception
+    raise RuntimeError(
+        f"Unable to find version string in {VERSIONFILE}") from exception
 
 
 setup(
-    name='transformlib',
-    author='Troels Lægsgaard',
+    name="transformlib",
+    author="Troels Lægsgaard",
     version=__version__,
     description=(
         "Enables the user to organize transformations of data as a regular Python package."
@@ -32,26 +33,32 @@ setup(
     url="https://github.com/laegsgaardTroels/transformlib",
     package_dir={"": "src"},
     packages=find_packages(
-        where='src',
-        include=['transformlib*'],
+        where="src",
+        include=["transformlib*"],
     ),
-    python_requires='>=3.10',
+    python_requires=">=3.10",
     install_requires=[],
     extras_require={
-        'dev': [
-            'flake8==3.8.3',
-            'jinja2==3.0.0',
-            'pytest-cov',
-            'pytest-pep8',
-            'pytest==8.0.2',
-            'sphinx==7.2.6',
-            'scikit-learn==1.4.1.post1',
-            'pandas==2.2.1',
-        ]
+        "all": [
+            "pandas>=2.2.1",
+        ],
+        "pandas": [
+            "pandas>=2.2.1",
+        ],
+        "dev": [
+            "flake8==3.8.3",
+            "jinja2==3.0.0",
+            "pytest-cov",
+            "pytest-pep8",
+            "pytest==8.0.2",
+            "sphinx==7.2.6",
+            "scikit-learn==1.4.1.post1",
+            "pandas==2.2.1",
+        ],
     },
     entry_points={
-        'console_scripts': [
-            'transform = transformlib.__main__:main',
+        "console_scripts": [
+            "transform = transformlib.__main__:main",
         ]
     },
 )
