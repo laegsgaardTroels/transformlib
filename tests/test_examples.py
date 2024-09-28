fa1c74ca261f6a55632b73ec1adfc7f6d76eafc3
Troels Laegsgaard <laegsgaardtroels@gmail.com>
2024-09-28 22:56:46 +0200

    Adds docstrings to tests

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -12,6 +12,7 @@ EXAMPLE_DIRS = list(pathlib.Path("./examples/").glob("*"))
 @pytest.mark.slow
 @pytest.mark.parametrize("example_dir", EXAMPLE_DIRS)
 def test_run_sh(example_dir: pathlib.Path, monkeypatch):
+    """Runs all pipeline.sh shell scripts in each example/*/pipeline.sh."""
     monkeypatch.chdir(example_dir)
     subprocess.run(["/bin/bash", "pipeline.sh"], check=True)
 
@@ -19,6 +20,7 @@ def test_run_sh(example_dir: pathlib.Path, monkeypatch):
 @pytest.mark.slow
 @pytest.mark.parametrize("example_dir", EXAMPLE_DIRS)
 def test_run_py(example_dir: pathlib.Path, tmp_path):
+    """Runs all pipeline.py python scripts in each example/*/pipeline.py."""
     pipeline = import_and_append_to_sys_path(example_dir / "pipeline.py")
     configure(data_dir=tmp_path)
     pipeline.pipeline.run()
