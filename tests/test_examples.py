ab203ccb1a10c7fdb4b54a19a2baed5dce494da6
Troels Laegsgaard <laegsgaardtroels@gmail.com>
2024-09-28 22:48:15 +0200

    Adds fasttests make target and marks slow tests

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -9,12 +9,14 @@ import subprocess
 EXAMPLE_DIRS = list(pathlib.Path("./examples/").glob("*"))
 
 
+@pytest.mark.slow
 @pytest.mark.parametrize("example_dir", EXAMPLE_DIRS)
 def test_run_sh(example_dir: pathlib.Path, monkeypatch):
     monkeypatch.chdir(example_dir)
     subprocess.run(["/bin/bash", "pipeline.sh"], check=True)
 
 
+@pytest.mark.slow
 @pytest.mark.parametrize("example_dir", EXAMPLE_DIRS)
 def test_run_py(example_dir: pathlib.Path, tmp_path):
     pipeline = import_and_append_to_sys_path(example_dir / "pipeline.py")
